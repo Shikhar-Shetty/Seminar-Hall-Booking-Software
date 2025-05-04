ad94de42e91d1f726199df0f8bf4cdd345a8d07e
Shikhar Shetty <shettyshikhar99@gmail.com>
2025-05-04 15:15:38 +0530

    Fixed a issue

--- lib/getUser.ts
+++ lib/getUser.ts
@@ -1,10 +1,11 @@
 import { getServerSession } from "next-auth";
 import dbConnect from "./dbConnect"
 import { revalidatePath } from "next/cache";
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 
 export const getUser = async() => {
     dbConnect();
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
     const userId = session?.user?.id;
     if(!userId){
         revalidatePath("/sign-in")
